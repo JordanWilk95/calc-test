79d8f9f8fb06e33f1709ac5441fff965e6450bef
Jordan <jwilk95@outlook.com>
2021-11-06 12:27:41 +0000

    Calc test

---
+++ README.md
@@ -0,0 +1,8 @@
+# Coin Calc test
+## Summary
+-
+## How to use
+- `npm start` 
+- http://localhost:4060/
+- enter a number: "£2.50", "250", "2.45" will all produce valid inputs
+- invalid inputs are eg: empty strings, "£p", "1x"
---
+++ app.js
@@ -0,0 +1,66 @@
+// hosting local server and calling express
+const express = require('express')
+const app = express()
+const PORT = 4060
+// making  a static index page
+app.use(express.static('index.html'))
+// making the app listen on a port
+app.listen(PORT, () => {
+    console.log(`Express server listening at http://localhost:${PORT}`)
+})
+//coin counter function
+var coinCounter = function (total) {
+    // taking user input to convert
+    var returnValue = total;
+    // used to display the total amount of coins
+    var totalcoins = {};
+
+    // values for the coins 
+    const coinTypes = ["pound", "fifty", "twenty", "ten", "five", "two", "one"]
+    const coinValues = [100, 50, 20, 10, 5, 2, 1]
+    //function for working out the amount
+    var amount;
+    for (var i = 0; i < coinValues.length; i++) {
+        amount = Math.floor(returnValue / coinValues[i]);
+        // if the amount is greater than 0 start to count the different amount of coins
+        if (amount > 0) {
+            // declaring that the total amount be displayed in coin types
+            totalcoins[coinTypes[i]] = amount;
+            // taking user input and matching it by the coin values to produce the amount of coin types
+            returnValue = returnValue % coinValues[i]
+        }
+    }
+    // end of the function displaying the amount of coins
+    return totalcoins;
+}
+
+
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// express endpoint
+app.get('/number', (req, res) => {
+    // takes user input
+    let x = req.query.bar
+    x = x.replace('p', '')
+
+    // if x include £ multiply by 100
+    if (x.includes('£')) {
+        // removing the £ from the user input
+        x = x.replace('£', '')
+        x = Number(x).toFixed(2)
+        x = x * 100
+    } else if (x.includes('.')) { // x is 2.50
+        x = Number(x).toFixed(2)
+        x = x.replace('.', '') // 250
+    }
+    if (isNaN(x) || ! x){
+        console.log(x)
+        return res.json("enter valid input")
+        
+    } 
+    //prints userinput to the json part on the numbers page
+    res.json(coinCounter(x))
+})
